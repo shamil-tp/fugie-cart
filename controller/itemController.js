bc998e779fc2b1475c669fd81cde751909068e82
sinansrk <sinansrk966@gmail.com>
2026-02-09 19:23:12 +0530

    done

--- controller/itemController.js
+++ controller/itemController.js
@@ -3,9 +3,27 @@ const Item = require('../models/Item');
 exports.getAllItems = async (req, res) => {
     try {
         const items = await Item.find();
+        // await Item.insertMany([
+        //     {
+        //         id: 'coffee01',
+        //         name: 'Coffee',
+        //         price: 2.5,
+        //         quantity: 100,
+        //         img: 'https://images.unsplash.com/photo-1509042239860-f550ce710b93'
+        //     },
+        //     {
+        //         id: 'tea01',
+        //         name: 'Tea',
+        //         price: 1.8,
+        //         quantity: 100,
+        //         img: 'https://images.unsplash.com/photo-1544787219-7f47ccb76574'
+        //     }
+        // ])
+
         res.status(200).json({
             success: true,
             items
+
         });
     } catch (error) {
         res.status(500).json({
@@ -14,6 +32,8 @@ exports.getAllItems = async (req, res) => {
         });
     }
 };
+
+
 
 exports.createItem = async (req, res) => {
     try {
